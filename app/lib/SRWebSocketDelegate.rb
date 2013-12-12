285371fc861afcbf4e634d2a9fe2ed10d413b600
Paul Sturgess <paulsturgess@Pauls-MacBook-Pro.local>
2013-12-12 11:20:22 +0000

    Pass all notifications through to AppDelegate
    Do not create notifications for every user vote

--- app/lib/SRWebSocketDelegate.rb
+++ app/lib/SRWebSocketDelegate.rb
@@ -4,12 +4,14 @@ class SRWebSocketDelegate
     jukebox = App.shared.delegate.jukebox
     jukebox.update!(msg.dataUsingEncoding(NSUTF8StringEncoding))
     jukebox.notifications.each do |message|
-      notification = NSUserNotification.alloc.init
-      notification.title = message.heading
-      notification.subtitle = message.subtitle
-      notification.informativeText = message.description
-
-      NSUserNotificationCenter.defaultUserNotificationCenter.scheduleNotification(notification)
+      App.shared.delegate.new_notification(message)
+      unless message.class == ::KyanJukebox::Rating # dirty way to prevent generating notifications for every new rating!
+        notification = NSUserNotification.alloc.init
+        notification.title = message.heading
+        notification.subtitle = message.subtitle
+        notification.informativeText = message.description
+        NSUserNotificationCenter.defaultUserNotificationCenter.scheduleNotification(notification)
+      end
     end
   end
 
